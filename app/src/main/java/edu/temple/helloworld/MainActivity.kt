5f16fa5e2edb32921cb986d68a06f7092aa90652
Gabriel Lantigua <143743234+glantig1@users.noreply.github.com>
2023-09-25 15:42:57 -0400

    Resonding to issue #2, a condition statement was added to check if text was added. If text was added then it will display the name, else it displays an error and doesn't change the display.

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var nameEdit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +18,11 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-
+        nameEdit = findViewById<EditText>(R.id.nameEditText)
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if(nameEdit.text.toString() != "") displayTextView.text = "Hello, ${nameEdit.text}"
+            else nameEdit.setError("Name not entered")
         }
-
-
     }
 }
